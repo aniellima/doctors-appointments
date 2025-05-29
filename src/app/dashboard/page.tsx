c990f7bfef0f5ad95fa0852299a8c46f965c6ee1
aniellima <77987670+aniellima@users.noreply.github.com>
2025-05-29 12:53:28 -0300

    feat: enhance authentication flow with session checks

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,29 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  return (
+    <div>
+      <div>DashboardPage</div>
+      <div>
+        <pre>{session?.user?.name}</pre>
+        <pre>{session?.user?.email}</pre>
+      </div>
+      <SignOutButton />
+    </div>
+  );
 };
 
 export default DashboardPage;
